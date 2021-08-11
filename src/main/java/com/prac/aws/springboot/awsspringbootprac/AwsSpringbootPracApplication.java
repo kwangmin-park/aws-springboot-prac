7caa293646eb7cff6efd344f245c24f85c0cd6bc
huiae07 <huiae07@gmail.mail>
2021-08-11 23:16:50 +0900

    Initial commit

---
+++ src/main/java/com/prac/aws/springboot/awsspringbootprac/AwsSpringbootPracApplication.java
@@ -0,0 +1,13 @@
+package com.prac.aws.springboot.awsspringbootprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringbootPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringbootPracApplication.class, args);
+	}
+
+}
