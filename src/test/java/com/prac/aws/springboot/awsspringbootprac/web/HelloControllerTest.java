e0ecff3a7657fa055728a4da3f716cf158766ed8
huiae07 <huiae07@gmail.mail>
2021-08-13 00:48:26 +0900

    helloDto 이용하여 json return 및 테스트

--- src/test/java/com/prac/aws/springboot/awsspringbootprac/web/HelloControllerTest.java
+++ src/test/java/com/prac/aws/springboot/awsspringbootprac/web/HelloControllerTest.java
@@ -7,9 +7,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 //@RunWith - 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
 //여기서는 SpringRunner라는 스프링 실행자를 실행
@@ -33,5 +35,21 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // /hello API를 GET방식으로 요청. 체이닝 지원.
                 .andExpect(status().isOk()) //HTTP Header의 status ok(200)인지 확인
                 .andExpect(content().string(hello)); //결과가 hello인지 확인
+    }
+
+    @Test
+    public void helloDto_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+//        param은 API 테스트할때 요청파라미터 설정. String만 허용하므로 int, date 등은 String 으로 변경 필요
+//        jsonPath는 JSon 응답값을 필드별로 검증할 수 있는 메소드. $. 을 기준으로 필드명 명시
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+
     }
 }
