3579b46252ffe343b25f625ab4abdccaeb4c0fcd
huiae07 <huiae07@gmail.mail>
2021-08-13 00:10:44 +0900

    test파일 생성 및 테스트 연습. intelliJ 에서 테스트할 때 gradle run 환경을 gradle -> IntelliJ IDEA로 변경 필요

--- src/main/java/com/prac/aws/springboot/awsspringbootprac/AwsSpringbootPracApplication.java
+++ src/main/java/com/prac/aws/springboot/awsspringbootprac/AwsSpringbootPracApplication.java
@@ -3,9 +3,11 @@ package com.prac.aws.springboot.awsspringbootprac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//SpringBoot 자동 설정, Bean 읽기 생성 자동 설정. 아래 Annotation  위치부터 설정을 읽어가기 때문에 항상 프로젝트의 최상단에 위치해야한다.
 @SpringBootApplication
 public class AwsSpringbootPracApplication {
 
+//	내장 WAS(Web Application Server) 실행. 서버에 톰캣을 설치할 필요 없고 스프링부트로 만등러진 Jar 파일로 실행가능 -> 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있다.
 	public static void main(String[] args) {
 		SpringApplication.run(AwsSpringbootPracApplication.class, args);
 	}
---
+++ src/main/java/com/prac/aws/springboot/awsspringbootprac/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.prac.aws.springboot.awsspringbootprac.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
+//각 메소드마다 @ResponseBody 어노테이션을 선언했던 것을 한번에 사용할 수 있게 만드는 역할
+@RestController
+public class HelloController {
+
+//    HTTP Method 중 Get 요청을 받을 수 있는 API로 만들어준다.
+//    GET 방식으로 /hello로 요청이 오면 문자열 hello를 반환하는 기능을 갖는다.
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
