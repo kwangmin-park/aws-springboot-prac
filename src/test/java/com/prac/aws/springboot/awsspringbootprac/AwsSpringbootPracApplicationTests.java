4c52744ff6823be106e6bd55c381117f51fe3152
huiae07 <huiae07@gmail.mail>
2021-09-20 01:11:19 +0900

    테스트 복구 및 travis 작업중

--- src/test/java/com/prac/aws/springboot/awsspringbootprac/AwsSpringbootPracApplicationTests.java
+++ src/test/java/com/prac/aws/springboot/awsspringbootprac/AwsSpringbootPracApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.prac.aws.springboot.awsspringbootprac;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class AwsSpringbootPracApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.prac.aws.springboot.awsspringbootprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSpringbootPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
